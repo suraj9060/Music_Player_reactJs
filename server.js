4c2351925a06d3b73a7ea8745c984ee241ecda58
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-02-20 00:15:28 +0530

    this is the backend of music app

---
+++ server.js
@@ -0,0 +1,78 @@
+const { json } = require("express");
+const { appData } = require("./mock")
+const cors = require("cors");
+const express = require("express")
+const fs = require("fs");
+// const { type } = require("os");
+const app = express()
+
+const port = 9000;
+
+
+
+const __AUDIO_TYPE__ = {
+  ROCK: "rock",
+  JAZZ: "jazz",
+  CINEMATIC: "cinematic",
+  ACCOUSTIC: "accoustic",
+};
+
+function getDirectories(type) {
+  return fs.readdirSync(`./music/${type}`);
+}
+
+function getSongDetails(type, dir) {
+    try {
+         const path = `./music/${type}/${dir}/media`;
+        const audioData = fs.readdirSync(path);
+        const audioInfo = fs.readFileSync(`./music/${type}/${dir}/info.json`)
+        const parsedDate = JSON.parse(audioInfo)
+
+         return {
+           audioFile: `${path}/${audioData[0]}`,
+           avatar: `${path}/${audioData[1]}`,
+           ...parsedDate,
+         };
+        
+    } catch (error) {
+        return false
+    }
+   
+}
+
+
+
+app.use('/music', express.static("music"))
+
+app.use(cors())
+
+app.get(`/song`, (req, res) => {
+
+  const songData = {};
+  
+    for (let type in __AUDIO_TYPE__) {
+        const directoryItems = (getDirectories(__AUDIO_TYPE__[type]));
+        directoryItems.forEach((item) => {
+            
+            const audioData = getSongDetails(__AUDIO_TYPE__[type], item)
+            if (audioData) {
+                console.log({ audioData });
+                if (__AUDIO_TYPE__[type] in songData) {
+                    songData[__AUDIO_TYPE__[type]].push(audioData)
+                } else {
+                    songData[__AUDIO_TYPE__[type]] = [audioData]
+                }
+            }
+            
+        })
+    }
+    console.log({ songData })
+
+    appData['freelicense'] = songData
+
+    res.status(200).json({ songData });
+});
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
